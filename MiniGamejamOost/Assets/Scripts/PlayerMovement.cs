dd7c675b75642b909c7a14da4c887e593125f0d1
LucasNijland <103414594+LucasNijland@users.noreply.github.com>
2023-02-09 11:09:45 +0100

    playerspeed accelerates

--- MiniGamejamOost/Assets/Scripts/PlayerMovement.cs
+++ MiniGamejamOost/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rigidbody;
-    private float playerSpeed = 5f;
+    private Rigidbody2D rb;
+    [SerializeField]private float playerSpeed = 5f;
     public GameObject bullet;
     public GameObject shootPoint;
-    private bool facingRight = true;
+    [SerializeField]private float speedMulti;
+    [SerializeField]private float timer = 1;
+    private float maxspeed;
+    float duration = 1;
+    float timer2;
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        timer2 = timer;
+        rb = GetComponent<Rigidbody2D>();
     }
 
 
@@ -23,15 +28,35 @@ public class PlayerMovement : MonoBehaviour
         {
             PlayerShoot();
         }
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer = timer2;
+            speedMulti = playerSpeed;
+            maxspeed = playerSpeed + speedMulti;
+            StartCoroutine(Test(playerSpeed, maxspeed));
+
+        }
     }
 
     void PlayerMove()
     {
-        rigidbody.velocity = new Vector2(1 * playerSpeed, rigidbody.velocity.y);
+        rb.velocity = new Vector2(1 * playerSpeed, rb.velocity.y);
     }
     void PlayerShoot()
     {
         Instantiate(bullet, shootPoint.transform.position, shootPoint.transform.rotation);
+    }
+    IEnumerator Test(float startValue, float endValue)
+    {
+        float time = 0;
+
+        while (time < duration)
+        {
+            playerSpeed = Mathf.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
     }
 
 }
