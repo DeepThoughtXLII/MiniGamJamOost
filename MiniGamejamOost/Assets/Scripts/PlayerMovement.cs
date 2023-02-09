9b7a8847d17c682d6ed0649be54bbe8c8d2ea706
LucasNijland <103414594+LucasNijland@users.noreply.github.com>
2023-02-09 15:12:37 +0100

    vanishing block and ammo block

--- MiniGamejamOost/Assets/Scripts/PlayerMovement.cs
+++ MiniGamejamOost/Assets/Scripts/PlayerMovement.cs
@@ -80,4 +80,12 @@ public class PlayerMovement : MonoBehaviour
         }
         
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.name == "AmmoBox")
+        {
+            gameManager.GetComponent<AmmoCache>().bulletAmount += 50;
+            Destroy(collision.collider.gameObject);
+        }
+    }
 }
